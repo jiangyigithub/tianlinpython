ab64e489a9ce12a83bd8511f735e77c40340d411
jiangyi <15216787356@163.com>
2022-05-25 22:41:49 +0800

    ++

--- 4chapter/magicians.py
+++ 4chapter/magicians.py
@@ -2,7 +2,4 @@ magicians=['alice','david','carolina']
 for magician in magicians:
     print(f"{magician.title()},that was a great trick!")
     print(f"I can't wait to see you next trick,{magician.title()}!\n")
-
-print("Thank you everyone.That was a great magic show!")
-
-    
+print("Thank you everyone,that was a great magic show!")
