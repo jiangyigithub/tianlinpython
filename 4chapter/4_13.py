b8cd5b9ef3397a3ea877951f6330fd0186e2da7c
jiangyi <15216787356@163.com>
2022-05-28 18:28:53 +0800

    ++

--- 4chapter/4_13.py
+++ 4chapter/4_13.py
@@ -1,6 +1,6 @@
 buffet_foods=('dumplings','hamburger','buns','chips','coca cola')
 for buffet_food in buffet_foods:
-    print(buffet_food.title()\n)
+    print(buffet_food.title() )
 
 buffet_foods=('economic','cycles','buns','chips','coca cola')
 for buffet_food in buffet_foods:
---
+++ 5chapter/5_1.py
@@ -0,0 +1,6 @@
+car='cetuan'
+print("Is car =='cetuan'? I ptedict Ture.")
+print(car=='cetuan')
+
+print("\n Is car == 'audi'? I predict False.")
+print(car=='audi')
